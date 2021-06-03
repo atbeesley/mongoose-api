c8a0dec68aece9d24ed9dd117f9a8e9713f26ed9
atbeesley <alexander.beesley@gmail.com>
2021-06-03 08:12:48 +0100

    add model Post

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const post = require('../../models/Post');
 
 router.get('/', (req, res) => {
 	res.send('this is posts');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
 });
 
 module.exports = router;
